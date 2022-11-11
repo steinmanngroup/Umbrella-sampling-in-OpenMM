608a28c68d5355812e3d5bc01f38f6534c68384a
Simon Nyggard-Thomsen <36227010+snyga@users.noreply.github.com>
2022-11-11 10:01:48 +0100

    Update A0.py

--- Umbrella_sampling/A0.py
+++ Umbrella_sampling/A0.py
@@ -2,8 +2,7 @@ from sys import argv
 
 
 from setup import set_initial_molecules, set_pars, set_paths
-# from setup import get_sub2, get_sub_AI3
-
+from setup import get_sub2, get_sub_herc3
 script, main_path = argv
 
 set_paths(main_path=main_path)
@@ -11,5 +10,5 @@ set_pars(main_path=main_path)
 set_initial_molecules(main_path)
 
 # Setting up sh files
-# get_sub2(main_path=main_path)
-# get_sub_AI3(main_path=main_path)
+get_sub2(main_path=main_path)
+get_sub_herc3(main_path=main_path)
